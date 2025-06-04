da19d6fa098a6a0f4ba21d4b3bca3066d6c2102b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-05 00:51:41 +0530

    creating custom ui colors

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
 
-  
+  static const primaryColor = Color(0XFFFFA8DADC)
+
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
