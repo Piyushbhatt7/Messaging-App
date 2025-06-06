9adb07cec2031ac4ffafe27a2cdc84c319f416fd
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-07 00:28:33 +0530

    usermodel

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
-
   final String uid;
   final String username;
   final String fullName;
@@ -11,5 +10,19 @@ class UserModel {
   final bool isOnline;
   final Timestamp lastseen;
   final Timestamp createdAt;
+  final String fmcToken;
+  final List<String> blockedUsers;
 
+  UserModel({
+    required this.uid,
+    required this.username,
+    required this.fullName,
+    required this.email,
+    required this.phoneNumber,
+    required this.isOnline,
+    required this.lastseen,
+    required this.createdAt,
+    required this.fmcToken,
+    required this.blockedUsers,
+  });
 }
