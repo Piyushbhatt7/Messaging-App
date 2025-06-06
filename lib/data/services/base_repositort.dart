266f78b7982ed459e116a5d493e2dfebfd66e4d3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:19:17 +0530

    base repo

--- lib/data/services/base_repositort.dart
+++ lib/data/services/base_repositort.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class BaseRepositort {
-  
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
