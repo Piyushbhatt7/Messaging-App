c9abb8d8976a605371de8426797f93dd1ded7e8e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:16:46 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Messenger App',
       theme: AppTheme.lightTheme,
-      home: SignupScreen(),
+      home: LoginScreen(),
     );
   }
 }
