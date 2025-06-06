0f71358ad1eb0936b62db762a4492d356e227365
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:50:25 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatt_app/config/theme/app_theme.dart';
+import 'package:chatt_app/data/services/service_locator.dart';
 import 'package:chatt_app/firebase_options.dart';
 import 'package:chatt_app/presentation/screens/auth/login_screen.dart';
 import 'package:chatt_app/presentation/screens/auth/signup_screen.dart';
@@ -7,6 +8,7 @@ import 'package:flutter/material.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
