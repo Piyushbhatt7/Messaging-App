b27b53cc78ac368a3a27bcbaa224d2832f5eaa0d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:34:04 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: ,
       debugShowCheckedModeBanner: false,
       title: 'Messenger App',
       theme: AppTheme.lightTheme,
