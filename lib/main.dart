66bdf2acffea0618e647d1e445cd5d0868e132c5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:51:34 +0530

    navigation page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:chatt_app/data/services/service_locator.dart';
 import 'package:chatt_app/firebase_options.dart';
 import 'package:chatt_app/presentation/screens/auth/login_screen.dart';
 import 'package:chatt_app/presentation/screens/auth/signup_screen.dart';
+import 'package:chatt_app/router/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: ,
+      navigatorKey: getIt<AppRouter>().navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Messenger App',
       theme: AppTheme.lightTheme,
