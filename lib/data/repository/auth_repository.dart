aea1cebf3238b6bf0b5df2395a987dd14307cd95
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-07 00:52:12 +0530

    blockedUsers

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,6 +1,10 @@
+import 'package:chatt_app/data/models/user_model.dart';
 import 'package:chatt_app/data/services/base_repository.dart';
 
 class AuthRepository extends BaseRepository{
   
-  
+ Future<UserModel> signup({
+
+  required String //2:18
+ })
 }
