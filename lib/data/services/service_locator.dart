73bf25f72b4086a43fb3336fcba57cab9da3fd81
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-06 23:46:59 +0530

    navigation page

--- lib/data/services/service_locator.dart
+++ lib/data/services/service_locator.dart
@@ -1,6 +1,8 @@
+import 'package:chatt_app/router/app_router.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 Future<void> setupServiceLocator () async {
-  
+  getIt.registerLazySingleton(() => AppRouter());
+  getIt.
 }
