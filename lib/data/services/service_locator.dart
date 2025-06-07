7d90663c540f22717d0d2960e1c6575ce0018559
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-07 12:47:29 +0530

    services

--- lib/data/services/service_locator.dart
+++ lib/data/services/service_locator.dart
@@ -1,8 +1,9 @@
+import 'package:chatt_app/data/repository/auth_repository.dart';
 import 'package:chatt_app/router/app_router.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 Future<void> setupServiceLocator () async {
   getIt.registerLazySingleton(() => AppRouter());
-  
+  getIt.registerLazySingleton(() => AuthRepository());
 }
