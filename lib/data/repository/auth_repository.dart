4503c97874703948e304f3074294ac2f861f1587
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-07 12:59:28 +0530

    auth repo

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,8 +1,6 @@
 import 'dart:developer';
-
 import 'package:chatt_app/data/models/user_model.dart';
 import 'package:chatt_app/data/services/base_repository.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository extends BaseRepository{
   
@@ -44,10 +42,49 @@ class AuthRepository extends BaseRepository{
     }
  }
 
+ Future<UserModel> signIn({
+
+  required String email,
+  required String password,
+
+ }) async {
+    try{
+        final userCredential = await auth.signInWithEmailAndPassword(
+            email: email, 
+            password: password
+            );
+            if(userCredential.user == null)
+            {
+                throw "User not found";
+            }
+ 
+    }
+    catch(e) {
+        log(e.toString());
+        rethrow;
+    }
+ }
+
  Future<void> saveUserData(UserModel user) async{
 
     try {
       await firestore.collection("users").doc(user.uid).set(user.toMap());
+    } catch (e) {
+      throw "Failed to save user data"; 
+    }
+ }
+
+
+  Future<UserModel> getUserData(String uid) async{
+
+    try {
+      final doc = await firestore.collection("users").doc(uid).get();
+
+      if(!doc.exists)
+      {
+        throw "User data not found";
+      }
+      return UserModel.fromFirestore(doc);
     } catch (e) {
       throw "Failed to save user data"; 
     }
