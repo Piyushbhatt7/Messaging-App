081c73287919c289d1c98f9cf679bd6c008103b5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-07 15:14:24 +0530

    auth state

---
+++ lib/logic/cubits/auth/auth_state.dart
@@ -0,0 +1,30 @@
+import 'package:chatt_app/data/models/user_model.dart';
+import 'package:equatable/equatable.dart';
+
+enum AuthStatus { initial, loading, authenticated, unauthenticated, error }
+
+class AuthState extends Equatable {
+  final AuthStatus status;
+  final UserModel? user;
+  final String error;
+  AuthState copyWith({
+    AuthStatus? status,
+    UserModel? user,
+    String? error,
+  }) {
+    return AuthState(
+      status: status ?? this.status,
+      user: user ?? this.user,
+      error: error ?? this.error,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, user, error];
+  AuthState({
+    this.status = AuthStatus.initial, 
+    this.user, 
+    required this.error
+
+    });
+}
