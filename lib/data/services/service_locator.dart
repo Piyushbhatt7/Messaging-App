021ca0dd07ee580a7bc0a57a17f67f96a7ab2d31
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-08 15:07:42 +0530

    service locator

--- lib/data/services/service_locator.dart
+++ lib/data/services/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:chatt_app/data/repository/auth_repository.dart';
+import 'package:chatt_app/logic/cubits/auth/auth_cubit.dart';
 import 'package:chatt_app/router/app_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -10,4 +11,5 @@ Future<void> setupServiceLocator () async {
   getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   getIt.registerLazySingleton(() => AuthRepository());
+  getIt.registerFactory(() => AuthCubit(authRepository: AuthRepository()));
 }
