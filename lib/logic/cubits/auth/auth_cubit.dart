181f6a4390a24199a7fd5dc1dba88b5180f0dd11
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-08 11:37:47 +0530

    cubit

--- lib/logic/cubits/auth/auth_cubit.dart
+++ lib/logic/cubits/auth/auth_cubit.dart
@@ -1,1 +1,17 @@
-                     
+import 'dart:async';
+
+import 'package:chatt_app/data/repository/auth_repository.dart';
+import 'package:chatt_app/logic/cubits/auth/auth_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AuthCubit extends Cubit<AuthState>{
+
+    final AuthRepository _authRepository;
+    StreamSubscription<User?>? _authStateSubscription;
+
+    AuthCubit({
+        required this._authRepository,
+        this._authStateSubscription,
+    })
+}
