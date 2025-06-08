84a524d44bfb81e808537cac80bf66952679fd8a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-08 22:22:32 +0530

    auth repo

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -4,116 +4,111 @@ import 'package:chatt_app/data/services/base_repository.dart';
 import 'package:chatt_app/logic/cubits/auth/auth_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthRepository extends BaseRepository{
-
+class AuthRepository extends BaseRepository {
   Stream<User?> get authStateChanges => auth.authStateChanges();
-  
- Future<UserModel> signUp({
 
-  required String fullName,
-  required String username,
-  required String email,
-  required String phoneNumber,
-  required String password,
+  Future<UserModel> signUp({
+    required String fullName,
+    required String username,
+    required String email,
+    required String phoneNumber,
+    required String password,
+  }) async {
+    try {
+      final formattedPhoneNumber = phoneNumber.replaceAll(
+        RegExp(r'\s+'),
+        "".trim(),
+      );
+      final userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user == null) {
+        throw "Failed to creat a user";
+      }
 
- }) async {
-    try{
+      // create a auser model and sav in db firestore
 
-        final formattedPhoneNumber = phoneNumber.replaceAll(RegExp(r'\s+'), "".trim());
-        final userCredential = await auth.createUserWithEmailAndPassword(
-            email: email, 
-            password: password
-            );
-            if(userCredential.user == null)
-            {
-                throw "Failed to creat a user";
-            }
+      final user = UserModel(
+        uid: userCredential.user!.uid,
+        username: username,
+        fullName: fullName,
+        email: email,
+        phoneNumber: formattedPhoneNumber,
+      );
 
-            // create a auser model and sav in db firestore
-
-            final user = UserModel(
-                uid: userCredential.user!.uid, 
-                username: username, 
-                fullName: fullName, 
-                email: email, 
-                phoneNumber: formattedPhoneNumber, 
-            );
-
-            await saveUserData(user);  
-            return user; 
+      await saveUserData(user);
+      return user;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
     }
-    catch(e) {
-        log(e.toString());
-        rethrow;
-    }
- }
-
- Future<UserModel> signIn({
-
-  required String email,
-  required String password,
-
- }) async {
-    try{
-
-        final userCredential = await auth.signInWithEmailAndPassword(
-            email: email, 
-            password: password
-            );
-            if(userCredential.user == null)
-            {
-                throw "User not found";
-            }
-        final userData = await getUserData(userCredential.user!.uid);
-        return userData;
-    }
-    catch(e) {
-        log(e.toString());
-        rethrow;
-    }
- }
-
- Future<bool> checkEmailExists(String email) async{
-
-  try {
-
-    final methods = await auth.fetchSignInMethodsForEmail(email);
-    return methods.isNotEmpty;
-
-  } catch (e) {
-    
-    print("Error checking email: $e",);
-    return false;
   }
- }
 
- Future<void> saveUserData(UserModel user) async{
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user == null) {
+        throw "User not found";
+      }
+      final userData = await getUserData(userCredential.user!.uid);
+      return userData;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
 
+  Future<bool> checkEmailExists(String email) async {
+    try {
+      final methods = await auth.fetchSignInMethodsForEmail(email);
+      return methods.isNotEmpty;
+    } catch (e) {
+      print("Error checking email: $e");
+      return false;
+    }
+  }
+
+
+  
+  Future<bool> checkPhoneNumberExists(String phoneNumber) async {
+    try {
+      final methods = await auth.fetchSignInMethodsForEmail(phoneNumber);
+      return methods.isNotEmpty;
+    } catch (e) {
+      print("Error checking email: $e");
+      return false;
+    }
+  }
+
+  Future<void> saveUserData(UserModel user) async {
     try {
       await firestore.collection("users").doc(user.uid).set(user.toMap());
     } catch (e) {
-      throw "Failed to save user data"; 
+      throw "Failed to save user data";
     }
- }
+  }
 
-  
-  Future<void> signOut ()async {
-
+  Future<void> signOut() async {
     await auth.signOut();
   }
 
-  Future<UserModel> getUserData(String uid) async{
-
+  Future<UserModel> getUserData(String uid) async {
     try {
       final doc = await firestore.collection("users").doc(uid).get();
 
-      if(!doc.exists)
-      {
+      if (!doc.exists) {
         throw "User data not found";
       }
       return UserModel.fromFirestore(doc);
     } catch (e) {
-      throw "Failed to save user data"; 
+      throw "Failed to save user data";
     }
- }
+  }
 }
