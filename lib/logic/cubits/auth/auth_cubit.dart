2baf8b3df54d678c64c50f54616ce47a680a8453
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-08 15:06:24 +0530

    auth cubit

--- lib/logic/cubits/auth/auth_cubit.dart
+++ lib/logic/cubits/auth/auth_cubit.dart
@@ -5,38 +5,32 @@ import 'package:chatt_app/logic/cubits/auth/auth_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AuthCubit extends Cubit<AuthState>{
+class AuthCubit extends Cubit<AuthState> {
+  final AuthRepository _authRepository;
+  StreamSubscription<User?>? _authStateSubscription;
 
-    final AuthRepository _authRepository;
-    StreamSubscription<User?>? _authStateSubscription;
+  AuthCubit({required AuthRepository authRepository})
+    : _authRepository = authRepository,
+      super(const AuthState()) {
+    _init();
+  }
 
-    AuthCubit({
-        required AuthRepository authRepository,
+  void _init() {
+    emit(state.copyWith(status: AuthStatus.initial));
 
-    }): _authRepository = authRepository, super( 
-        const AuthState()) {
-        _init();
+    _authStateSubscription = _authRepository.authStateChanges.listen((
+      user,
+    ) async {
+      if (user != null) {
+        try {
+          final userData = await _authRepository.getUserData(user.uid);
+          emit(state.copyWith(status: AuthStatus.authenticated, user: userData),);
+        } catch (e) {
+          emit(state.copyWith(status: AuthStatus.error, error: e.toString()));
         }
-
-    void _init() {
-        emit(state.copyWith(status: AuthStatus.initial));
-
-        _authStateSubscription = _authRepository.authStateChanges.listen((user)
-        async{
-
-            if(user!=null)
-            {
-                try {
-                  final userData = await _authRepository.getUserData(user.uid);
-                  emit(state.copyWith(status: AuthStatus.authenticated));
-                } catch (e) {
-                  emit(state.copyWith(status: AuthStatus.error, error: e.toString()));
-                }
-            }
-
-            else {
-                emit(state.copyWith(status: AuthStatus.authenticated));
-            }
-        });
-    }
+      } else {
+        emit(state.copyWith(status: AuthStatus.authenticated));
+      }
+    });
+  }
 }
