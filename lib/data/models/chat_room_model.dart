c8130e1572251346915305ff808921246f48e207
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 23:03:47 +0530

    Map<String, String>?

--- lib/data/models/chat_room_model.dart
+++ lib/data/models/chat_room_model.dart
@@ -1,11 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatRoomModel {
-
   final String id;
   final List<String> participants;
   final String? lastMessage;
-  final String lastMessageSenderId;
+  final String? lastMessageSenderId;
   final Timestamp? lastMessageTime;
   final Map<String, Timestamp>? lastReadTime;
   final Map<String, String>? participantsName;
@@ -13,5 +12,16 @@ class ChatRoomModel {
   final String? isTypingUserId;
   final bool isCallActive;
 
-  ChatRoomModel({required this.id, required this.participants, required this.lastMessage, required this.lastMessageSenderId, required this.lastMessageTime, required this.lastReadTime, required this.participantsName, required this.isTyping, required this.isTypingUserId, required this.isCallActive});
+  ChatRoomModel({
+    required this.id,
+    required this.participants,
+    this.lastMessage,
+    this.lastMessageSenderId,
+    this.lastMessageTime,
+    Map<String, String>? lastReadTime,
+    Map<String, String>? participantsName,
+    required this.isTyping,
+    required this.isTypingUserId,
+    required this.isCallActive,
+  });
 }
