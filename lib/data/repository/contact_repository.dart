9cf1523bcc741964f7064bd3d693638f6de5df1a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 12:58:39 +0530

    contact repo

--- lib/data/repository/contact_repository.dart
+++ lib/data/repository/contact_repository.dart
@@ -1,5 +1,11 @@
 import 'package:chatt_app/data/services/base_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactRepository extends BaseRepository{
-  
+  String get currentUserId => FirebaseAuth.instance.currentUser?.uid ?? "";
+
+  Future<bool> requestContactsPermission() async {
+    return await FlutterContacts.requestPermission();
+  }
 }
