8f5e664e2954cb17b8b61113f7ee98d21a59f637
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-10 01:42:55 +0530

    Add initial implementation of MessageType and MessageStatus enums in ChatMessage model

--- lib/data/models/chat_message.dart
+++ lib/data/models/chat_message.dart
@@ -1,9 +1,17 @@
-enum MessageType {
-  text, image, video
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-enum MessageStatus { sent, read}
+enum MessageType { text, image, video }
+
+enum MessageStatus { sent, read }
 
 class ChatMessage {
-  
+  final String id;
+  final String chatRoomId;
+  final String senderId;
+  final String receiverId;
+  final String content;
+  final MessageType type;
+  final MessageStatus status;
+  final Timestamp timestamp;
+  final List<String> readBy;
 }
