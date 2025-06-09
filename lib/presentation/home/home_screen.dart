15d53855752746d5e36d08a461bae48147c7ecaf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 22:51:56 +0530

    home screen

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -75,6 +75,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: Text(contact["name"][0].toUperCase()),
                       ),
                       title: Text(contact["name"]),
+                      onTap: () {
+                        
+                      },
                     );
                   });
                 }
