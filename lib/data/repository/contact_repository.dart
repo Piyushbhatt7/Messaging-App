2be33636080116757b8e60e554d3b72003d3f557
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 13:12:56 +0530

    contact repo

--- lib/data/repository/contact_repository.dart
+++ lib/data/repository/contact_repository.dart
@@ -8,4 +8,32 @@ class ContactRepository extends BaseRepository{
   Future<bool> requestContactsPermission() async {
     return await FlutterContacts.requestPermission();
   }
+
+  Future<List<Map<String, dynamic>>> getRegisteredContacts() async {
+    try {
+      
+      // get the device contact with phone no.
+
+      final contacts = await FlutterContacts.getContacts(
+        withProperties: true,
+        withPhoto: true,
+      );
+
+      // extract phone no. and normalize them
+
+      final phoneNumbers = contacts
+      .where((contact) => contact.phones.isNotEmpty)
+    .map((contact) => {
+      "name": contact.displayName,
+      "phoneNumber": contact.phones.first.number.replaceAll(RegExp(r'[^\d+]'), ''),
+      "photo": contact.photo,
+    }).toList();
+
+      // get all users from firestore
+
+      // match contacts with registered users
+    } catch (e) {
+      
+    }
+  }
 }
