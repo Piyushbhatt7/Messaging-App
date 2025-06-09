ef2d837681a00c71e6489980f81fa33fdf7736c4
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-10 01:13:00 +0530

    Add ChatRepository class with initial implementation

--- lib/data/repository/chat_repository.dart
+++ lib/data/repository/chat_repository.dart
@@ -2,22 +2,29 @@ import 'package:chatt_app/data/models/chat_room_model.dart';
 import 'package:chatt_app/data/services/base_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatRepository extends BaseRepository{
-
+class ChatRepository extends BaseRepository {
   CollectionReference get _chatTooms => firestore.collection("chatRooms");
 
-  Future<ChatRoomModel> getOrCreateChatRoom(String currentUserId, String otherUserId) async{
-     final users = [currentUserId, otherUserId]..sort();
-     // abcd // xyz
-     final roomId = users.join("_");
+  Future<ChatRoomModel> getOrCreateChatRoom(
+    String currentUserId,
+    String otherUserId,
+  ) async {
+    final users = [currentUserId, otherUserId]..sort();
+    // abcd // xyz
+    final roomId = users.join("_");
 
-     final roomDoc = await _chatTooms.doc(roomId).get();
+    final roomDoc = await _chatTooms.doc(roomId).get();
 
-     if(roomDoc.exists)
-     {
+    if (roomDoc.exists) {
       return ChatRoomModel.fromFirestore(roomDoc);
-     }
+    }
 
-     final currentUserData = (await firestore.collection("users").doc(currentUserId).get()).data() as Map<String, dynamic>;
+    final currentUserData =
+        (await firestore.collection("users").doc(currentUserId).get()).data()
+            as Map<String, dynamic>;
+
+    final otherUserData =
+        (await firestore.collection("users").doc(currentUserId).get()).data()
+            as Map<String, dynamic>;
   }
 }
