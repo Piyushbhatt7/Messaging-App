ecb782f362aad5c77574ade5ff00619d1a6a7664
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 23:01:26 +0530

    Add initial Chat RoomModel class structure

--- lib/data/models/chat_room_model.dart
+++ lib/data/models/chat_room_model.dart
@@ -7,5 +7,11 @@ class ChatRoomModel {
   final String? lastMessage;
   final String lastMessageSenderId;
   final Timestamp? lastMessageTime;
-  final Map<String, Timestamp>? lastRead;
+  final Map<String, Timestamp>? lastReadTime;
+  final Map<String, String>? participantsName;
+  final bool isTyping;
+  final String? isTypingUserId;
+  final bool isCallActive;
+
+  ChatRoomModel({required this.id, required this.participants, required this.lastMessage, required this.lastMessageSenderId, required this.lastMessageTime, required this.lastReadTime, required this.participantsName, required this.isTyping, required this.isTypingUserId, required this.isCallActive});
 }
