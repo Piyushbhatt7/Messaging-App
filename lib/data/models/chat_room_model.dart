fa465fce89dc07209f80eb8c8b9b22e906864378
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-09 22:57:20 +0530

    Add initial Chat RoomModel class structure

--- lib/data/models/chat_room_model.dart
+++ lib/data/models/chat_room_model.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatRoomModel {
 
   final String id;
   final List<String> participants;
   final String? lastMessage;
-  final String  ;
+  final String lastMessageSenderId;
+  final Timestamp? lastMessageTime;
+  final Map<String, Timestamp>? lastRead;
 }
