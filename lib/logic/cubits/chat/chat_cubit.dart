6eeebad7a3a1885f6258a4c124329b5d9e857e9d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 13:40:44 +0530

    chat cubit

---
+++ lib/logic/cubits/chat/chat_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:chatt_app/data/repository/chat_repository.dart';
+import 'package:chatt_app/logic/cubits/chat/chat_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  final ChatRepository _chatRepository;
+  final String currentUserId;
+
+  ChatCubit({
+    required ChatRepository chatRepository,
+    required this.currentUserId,
+  }) : _chatRepository = chatRepository;
+}
