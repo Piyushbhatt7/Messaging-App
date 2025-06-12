5dbb67dbe6adb616185ecc1aa4cd546dcadf30da
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 14:30:24 +0530

    and sevices to locator

--- lib/data/services/service_locator.dart
+++ lib/data/services/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:chatt_app/data/repository/auth_repository.dart';
+import 'package:chatt_app/data/repository/chat_repository.dart';
 import 'package:chatt_app/data/repository/contact_repository.dart';
 import 'package:chatt_app/logic/cubits/auth/auth_cubit.dart';
+import 'package:chatt_app/logic/cubits/chat/chat_cubit.dart';
 import 'package:chatt_app/router/app_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,4 +16,5 @@ Future<void> setupServiceLocator () async {
   getIt.registerLazySingleton(() => AuthRepository());
   getIt.registerLazySingleton(() => ContactRepository());
   getIt.registerLazySingleton(() => AuthCubit(authRepository: AuthRepository()));
+  getIt.registerLazySingleton(() => ChatCubit(chatRepository: ChatRepository(), currentUserId: getIt<FirebaseAuth>().currentUser!.uid));
 }
