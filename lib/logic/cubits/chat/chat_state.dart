37792be301c580f53b28ace9212c1ceadbb5d947
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 13:16:18 +0530

    chat state

--- lib/logic/cubits/chat/chat_state.dart
+++ lib/logic/cubits/chat/chat_state.dart
@@ -1,12 +1,29 @@
 import 'package:equatable/equatable.dart';
 
-enum ChatStatus{
-  initaial, loading, loaded, error
-}
+enum ChatStatus { initaial, loading, loaded, error }
 
-class ChatState extends Equatable{
-
+class ChatState extends Equatable {
   final ChatStatus status;
   final String? error;
   final String receiverId;
+
+  ChatState({
+    this.status,
+    required this.error,
+    required this.receiverId,
+  });
 }
+ChatState copyWith({
+  ChatStatus? status,
+  String? error,
+  String? receiverId,
+}) {
+  return ChatState(
+    status: status ?? this.status,
+    error: error ?? this.error,
+    receiverId: receiverId ?? this.receiverId,
+  );
+}
+
+@override
+List<Object?> get props => [status, error, receiverId];
