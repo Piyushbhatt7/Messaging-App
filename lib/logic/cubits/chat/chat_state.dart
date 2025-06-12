39d0847769998295c9a4931e631afea86ce52077
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 13:12:59 +0530

    chat state

---
+++ lib/logic/cubits/chat/chat_state.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+enum ChatStatus{
+  initaial, loading, loaded, error
+}
+
+class ChatState extends Equatable{
+
+  final ChatStatus status;
+  final 
+}
