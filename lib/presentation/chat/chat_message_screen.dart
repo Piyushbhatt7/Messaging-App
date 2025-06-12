a845e9edf5acf8d4bd2b94e7a4e5b96c7bfaaaf3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 14:31:54 +0530

    chat messages

--- lib/presentation/chat/chat_message_screen.dart
+++ lib/presentation/chat/chat_message_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatt_app/data/models/chat_message.dart';
+import 'package:chatt_app/data/services/service_locator.dart';
 import 'package:chatt_app/logic/cubits/chat/chat_cubit.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -21,8 +22,10 @@ class _ChatMessageScreenState extends State<ChatMessageScreen> {
   final  TextEditingController messageController = TextEditingController();
   late final ChatCubit _chatCubit;
 
-  @override
+  @override 
   void initState() {
+    _chatCubit = getIt<ChatCubit>();
+    _chatCubit.enterChat(widget.receiverId);
     // TODO: implement initState
     super.initState();
     
