79c4870da9a102eac1df78b845244e18cee94219
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-12 22:17:29 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:chatt_app/config/theme/app_theme.dart';
 import 'package:chatt_app/data/services/service_locator.dart';
 import 'package:chatt_app/firebase_options.dart';
+import 'package:chatt_app/logic/cubits/auth/auth_cubit.dart';
+import 'package:chatt_app/logic/cubits/auth/auth_state.dart';
 import 'package:chatt_app/presentation/chat/chat_message_screen.dart';
 import 'package:chatt_app/presentation/home/home_screen.dart';
 import 'package:chatt_app/presentation/screens/auth/login_screen.dart';
@@ -8,6 +10,7 @@ import 'package:chatt_app/presentation/screens/auth/signup_screen.dart';
 import 'package:chatt_app/router/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,6 +36,22 @@ class MyApp extends StatelessWidget {
         receiverId: 'sampleReceiverId',
         receiverName: 'Sample Receiver',
       ),
+       // BlocBuilder<AuthCubit, AuthState>(
+      //     bloc: getIt<AuthCubit>(),
+      //     builder: (context, state) {
+      //       if (state.status == AuthStatus.initial) {
+      //         return const Scaffold(
+      //           body: Center(child: CircularProgressIndicator()),
+      //         );
+      //       }
+      //       // You can customize the following logic as needed
+      //       // For demonstration, redirect to HomeScreen if authenticated, else LoginScreen
+      //       if (state.status == AuthStatus.authenticated) {
+      //         return const HomeScreen();
+      //       }
+      //         return const LoginScreen();
+      //     },
+      // ),
     );
   }
 }
