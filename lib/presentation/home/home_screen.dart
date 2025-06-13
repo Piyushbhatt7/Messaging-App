d69d5aa74b4b2e7e704d5bb3604fd80ee823f64f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-13 15:29:53 +0530

    home screen

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -17,7 +17,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   late final ContactRepository _contactRepository;
   late final ChatRepository _chatRepository;
   late final String _currentUserId;
@@ -33,121 +32,126 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _showContctList(BuildContext context) // 4:26
   {
-    showModalBottomSheet(context: context, builder: (context)
-    {
-      return Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text("Contacts", style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),),
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text(
+                "Contacts",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
 
-            Expanded(
-              child: FutureBuilder<List<Map<String, dynamic>>>(
-                future: _contactRepository.getRegisteredContacts(), 
-                builder: (context, snapshot)
-                {
-                  if(snapshot.hasError)
-                  {
-                    return Center(
-                      child: Text("Error: ${snapshot.error}"),
-                    );
-                  }
-              
-                  if(!snapshot.hasData){
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-              
-                  final contacts = snapshot.data!;
-              
-                  if(contacts.isEmpty)
-                  {
-                    return const Center(
-                      child: Text(
-                        "No contacts found"
-                      ),
-                    );
-                  }
-                  return ListView.builder(
-                    itemCount: contacts.length,
-                    itemBuilder: (context, index)
-                  {
-                    final contact = contacts[index];
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Theme.of(context).primaryColor.withOpacity(0.1),
-                        child: Text(contact["name"][0]),
-                                     // child: Text(widget.receiverName[0].toUpperCase()),
+              Expanded(
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: _contactRepository.getRegisteredContacts(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(child: Text("Error: ${snapshot.error}"));
+                    }
 
-                      ),
-                      title: Text(contact["name"]),
-                      onTap: () {
-                        getIt<AppRouter>().push(ChatMessageScreen(receiverId: contact['id'], receiverName: contact['name']));
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    final contacts = snapshot.data!;
+
+                    if (contacts.isEmpty) {
+                      return const Center(child: Text("No contacts found"));
+                    }
+                    return ListView.builder(
+                      itemCount: contacts.length,
+                      itemBuilder: (context, index) {
+                        final contact = contacts[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).primaryColor.withOpacity(0.1),
+                            child: Text(contact["name"][0]),
+
+                            // child: Text(widget.receiverName[0].toUpperCase()),
+                          ),
+                          title: Text(contact["name"]),
+                          onTap: () {
+                            getIt<AppRouter>().push(
+                              ChatMessageScreen(
+                                receiverId: contact['id'],
+                                receiverName: contact['name'],
+                              ),
+                            );
+                          },
+                        );
                       },
                     );
-                  });
-                }
+                  },
                 ),
-            )
-          ],
-        ),
-      );
-    });
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("Chats"),
-      actions: [
-        InkWell(
-          onTap: () async{
-           await getIt<AuthCubit>().signOut();
-           getIt<AppRouter>().pushAndRemoveUntil(const LoginScreen());
-          },
-          child: Icon(Icons.logout))
-      ],
-    ),
-      body: StreamBuilder(stream: _chatRepository.getChatRooms(_currentUserId), builder: (context, snapshot)
-      {
-        if(snapshot.hasError) {
-          print(snapshot.error);
-          return Center(
-            child: Text(
-              "error: ${snapshot.error}"
-            ),
+      appBar: AppBar(
+        title: Text("Chats"),
+        actions: [
+          InkWell(
+            onTap: () async {
+              await getIt<AuthCubit>().signOut();
+              getIt<AppRouter>().pushAndRemoveUntil(const LoginScreen());
+            },
+            child: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: _chatRepository.getChatRooms(_currentUserId),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return Center(child: Text("error: ${snapshot.error}"));
+          }
+
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final chats = snapshot.data!;
+
+          if (chats.isEmpty) {
+            return Center(child: Text("No recent Chats!"));
+          }
+          return ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (context, index) {
+              final chat = chats[index];
+              return ChatListTile(
+                chat: chat,
+                currentUserId: _currentUserId,
+                onTap: () {
+                  final otherUserId = chat.participants.firstWhere((id) => id != _currentUserId);
+                  final otherUserName = chat.participantsName ! [otherUserId] ?? "Unknow";
+                  getIt<AppRouter>().push(ChatMessageScreen(receiverId: otherUserId, receiverName: otherUserName));
+                },
+              );
+            },
           );
-        }
+        },
+      ),
 
-        if(!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-      final chats =snapshot.data!; 
-
-      if(chats.isEmpty)
-      {
-        return Center(
-          child: Text("No recent Chats!"),
-        );
-
-      }
-        return ListView.builder(itemBuilder: (context, index)
-        {
-          return ChatListTile(chat: chats[index], currentUserId: currentUserId, onTap: onTap)
-        });
-      }),
-
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        _showContctList(context);
-      },
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      child: Icon(Icons.chat_rounded, color: Colors.white,),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showContctList(context);
+        },
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: Icon(Icons.chat_rounded, color: Colors.white),
       ),
     );
   }
