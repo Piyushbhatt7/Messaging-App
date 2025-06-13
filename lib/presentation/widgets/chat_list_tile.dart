b19ee3ac1027010722bd788f6ac9bb6148b4c7f7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-13 14:51:28 +0530

    chat list

---
+++ lib/presentation/widgets/chat_list_tile.dart
@@ -0,0 +1,16 @@
+import 'package:chatt_app/data/models/chat_room_model.dart';
+import 'package:flutter/material.dart';
+
+class ChatListTile extends StatelessWidget {
+
+  final ChatRoomModel chat;
+  final String currentUserId;
+  final VoidCallback onTap;
+  
+  const ChatListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
