599222247af64ac2a152d8004275933c510868ae
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-16 15:38:58 +0530

    app updation

--- lib/logic/observer/app_life_cycle_obsever.dart
+++ lib/logic/observer/app_life_cycle_obsever.dart
@@ -1,3 +1,11 @@
-class AppLifeCycleObsever {
-  
+import 'package:chatt_app/data/repository/chat_repository.dart';
+import 'package:flutter/widgets.dart';
+
+class AppLifeCycleObsever extends WidgetsBindingObserver{
+
+  final String userId;
+  final ChatRepository chatRepository;
+
+  AppLifeCycleObsever({required this.userId, required this.chatRepository});
 }
+
