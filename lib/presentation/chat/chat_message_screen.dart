e52c6eec3f1a0926c0e02e2e1b6e6588556f5f25
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-17 15:31:12 +0530

    chat message

--- lib/presentation/chat/chat_message_screen.dart
+++ lib/presentation/chat/chat_message_screen.dart
@@ -69,7 +69,7 @@ class _ChatMessageScreenState extends State<ChatMessageScreen> {
                 ),
 
                  BlocBuilder<ChatCubit, ChatState>(
-                  bloc: getIt<ChatCubit>(),
+                  bloc: _chatCubit,
                   builder: (context, state)
                  {
                   if(state.isReceiverTyping)
