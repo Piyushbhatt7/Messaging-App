4af2e4d57bd6643fea68c112c4fb18c5f2d4a46d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-19 22:20:03 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyAppState extends State<MyApp> {
         title: 'Messenger App',
         theme: AppTheme.lightTheme, 
         home:   
-        // 
+        
          BlocBuilder<AuthCubit, AuthState>( 
             bloc: getIt<AuthCubit>(),  
             builder: (context, state) {  
