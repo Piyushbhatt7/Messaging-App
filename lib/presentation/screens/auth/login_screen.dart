4e9c832be872d5b0dae62714a95962d69c85b1d2
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-19 22:28:39 +0530

    Login Screen

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -180,7 +180,6 @@ class _LoginScreenState extends State<LoginScreen> {
                               recognizer:
                                   TapGestureRecognizer()
                                     ..onTap = () {
-  
                                       getIt<AppRouter>().push(
                                         const SignupScreen(),
                                       );
