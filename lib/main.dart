7fd95970dff6582ffb51e12090a539ceb3a3dacd
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-19 22:19:21 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyAppState extends State<MyApp> {
         theme: AppTheme.lightTheme, 
         home:   
         // ChatMessageScreen(
-        //   
+        //
          BlocBuilder<AuthCubit, AuthState>( 
             bloc: getIt<AuthCubit>(),  
             builder: (context, state) {  
